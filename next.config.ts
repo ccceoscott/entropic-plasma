b3a23fb5d31c227c852967d831734ff4fddddbb8
ccceoscott <scott@constantconcepts.io>
2026-03-19 17:11:16 -0700

    chore: enforce global V8 memory clamps in package pipelines [Infinity Protocol v6.2.14]

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  productionBrowserSourceMaps: false,
+  experimental: {
+    memoryBasedWorkersCount: true
+  }
 };
 
 export default nextConfig;
